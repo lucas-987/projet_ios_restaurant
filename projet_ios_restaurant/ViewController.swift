61ded45606975dd74fa38a53cd4dc9fd8a1cbd53
JasonLeMyope <jason.vandaele@edu.univ-fcomte.fr>
2021-01-25 10:02:42 +0100

    Initial Commit

---
+++ projet_ios_restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projet_ios_restaurant
+//
+//  Created by vandaele jason on 25/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
