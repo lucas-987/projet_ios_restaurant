33f38646368198b08067c3f1166617a9c8320065
lucas-987 <lptb68720@gmai.com>
2021-02-01 08:50:38 +0100

    Ajout de l'objet Dish.

---
+++ projet_ios_restaurant/Dish.swift
@@ -0,0 +1,19 @@
+//
+//  Dish.swift
+//  projet_ios_restaurant
+//
+//  Created by treyer lucas on 25/01/2021.
+//
+
+import Foundation
+
+struct Dish {
+    var id: Int
+    var name: String
+    var description: String
+    var price: Double
+    var calories: Int
+    var proteins: String
+    var carbs: String
+    var imageUrl: String
+}
